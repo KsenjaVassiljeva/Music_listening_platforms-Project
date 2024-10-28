1e74b595d06331fb256f6a8e0a18ac57d92f7b58
KsenjaVassiljeva <145933696+KsenjaVassiljeva@users.noreply.github.com>
2024-10-28 15:09:51 +0200

    Добавлен сайт HTML/CSS

--- models/userInteractions.js
+++ models/userInteractions.js
@@ -1,18 +1,35 @@
-const db = require('../config/database.js');
-const { DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/database'); // Путь к вашему файлу конфигурации базы данных
 
-const UserInteractions = db.define('userInteractions', {
-    favorited: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false
-    },
-    playedAt: {
-        type: DataTypes.DATE,
-        allowNull: false
-    }
+class UserInteraction extends Model {}
+
+UserInteraction.init({
+  id: {
+    type: DataTypes.BIGINT,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  track_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  favorite: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  played_at: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
 }, {
-    tableName: 'user_interactions',
-    timestamps: true
+  sequelize,
+  modelName: 'UserInteraction',
+  tableName: 'user_interactions', // Убедитесь, что это соответствует вашему имени таблицы в базе данных
+  timestamps: true, // Если вам нужны поля createdAt и updatedAt
 });
 
-module.exports = UserInteractions;
+module.exports = UserInteraction;
